71a9f1b518c8520f932d8bad9814bff986f96959
Imam Hossain Shibly <shibly.eeeruet@gmail.com>
2026-09-17 23:34:55 +0600

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DUTY_CONFIG = Object.freeze({
-  feedUrl: https://script.google.com/macros/s/AKfycbxpbfY9YLzxU39NWBXjfk6ye7cCr8NPAMi0unnqvdGaSiNVMnaps764GxX_LICvTpvw/exec
+  feedUrl: 'https://script.google.com/macros/s/AKfycbxpbfY9YLzxU39NWBXjfk6ye7cCr8NPAMi0unnqvdGaSiNVMnaps764GxX_LICvTpvw/exec'
   refreshMs: 120000
 });
